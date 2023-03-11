5561c3e529ef9e725b96c251b83001094bd5a3a5
EugeniaSkalnaya <ommitsu@gmail.com>
2023-03-11 18:40:28 +0300

    added fillarray and printarray methods

--- Program.cs
+++ Program.cs
@@ -3,3 +3,33 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше string[] 
 // array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] GivenArray = new string[] { "123", "23", "hello", "world", "res", "checklength", "=)" };
+
+void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int lengthLimit = GetNumber("Введите длину-ограничитель элемента массива");
+
+FillNewArray(GivenArray, newShortArray, lengthLimit);
+PrintArray(newShortArray);
